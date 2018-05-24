53b6e75f22111be02a72340a6436cf19732503b2
donkelley <dbkelley@gmail.com>
2018-05-24 16:10:23 -0700

    adding zipped constants for privacy

--- app/config/constants.js
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_API_KEY = "AIzaSyCcS93KU9Rd9PpuO7YlboOuBZzFkN8a8dU";
-export const FIREBASE_AUTH_DOMAIN = "mypopularity-d4681.firebaseapp.com";
-export const FIREBASE_DATABASE_URL = "https://mypopularity-d4681.firebaseio.com";
-export const FIREBASE_PROJECT_ID = "mypopularity-d4681";
-export const FIREBASE_STORAGE_BUCKET = "mypopularity-d4681.appspot.com";
-export const FIREBASE_MESSAGING_SENDER_ID = "571205797810";
-
-export const FACEBOOK_APP_ID = "176660776366496";
